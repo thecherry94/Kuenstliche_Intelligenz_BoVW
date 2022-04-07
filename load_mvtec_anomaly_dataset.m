e7f416f96c4fae11d24e4f81f94eb0a40260eb2c
thecherry94 <philipp.kirsch94@googlemail.com>
2022-04-07 11:59:08 +0200

    print hinzugefügt

--- load_mvtec_anomaly_dataset.m
+++ load_mvtec_anomaly_dataset.m
@@ -1,11 +1,11 @@
-function [] = load_mvtec_anomaly_dataset(directory)
+function dataset = load_mvtec_anomaly_dataset(directory)
     contents = dir(directory);
     dirFlags = [contents.isdir];
     subFolders = contents(dirFlags);
-
-    datasets = [];
     for i = 3 : length(subFolders)
-        
+        subdir = subFolders(i);
+        fprintf('loading defect type "%s" (%d/%d)', subdir.name, i - 2, length(subFolders));
+        dataset(end+1) = load_mvtec_anomaly_dataset_subdirectory(subdir);
     end
 end
 
