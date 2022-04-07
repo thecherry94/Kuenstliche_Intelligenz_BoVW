bd0be24f5496a7aaa6b84aaca8f53db0aeb0eecb
thecherry94 <philipp.kirsch94@googlemail.com>
2022-04-07 12:00:00 +0200

    Grundgerüst für subdirectories hinzugefügt

--- load_mvtec_anomaly_dataset_subdirectory.m
+++ load_mvtec_anomaly_dataset_subdirectory.m
@@ -1,6 +1,12 @@
 function [name, ground_truth, test, train] = load_mvtec_anomaly_dataset_subdirectory(directory)
     name = directory.name;
-    
-    
+    contents = dir(directory);
+    dirFlags = [contents.isdir];
+    subFolders = contents(dirFlags);
+
+    for i = 3 : length(subFolders)
+        subdir = subFolders(i);
+        
+    end
 end
 
