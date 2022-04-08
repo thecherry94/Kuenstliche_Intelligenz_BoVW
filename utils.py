b41eddd24f39140e1e82f2a4015aa56d8cd4fa3e
thecherry94 <philipp.kirsch94@googlemail.com>
2022-04-08 02:03:44 +0200

    Funktion 'load_mvtec_dataset' lädt jetzt alle Bilder mit Labels für das angegebene Objekt

--- utils.py
+++ utils.py
@@ -2,6 +2,16 @@ import os
 import cv2
 import numpy as np
 
+def load_mvtec_dataset(directory, object_type, resize_dim=0):
+    """
+    Loads the MVTEC dataset of the specified object type. (bottle, cable, capsule, etc.)
+    """
+
+    train_paths, test_paths, ground_truth_paths = load_mvtec_dataset_paths(directory, object_type)
+    train_images, train_labels, test_images, test_labels = load_and_label_data((train_paths, test_paths), resize_dim)
+    return train_images, train_labels, test_images, test_labels, ground_truth_paths
+
+
 def load_mvtec_dataset_paths(directory, object_type):
     """
     Loads the MVTEC dataset of the specified object type. (bottle, cable, capsule, etc.)
@@ -38,7 +48,40 @@ def load_mvtec_dataset_paths(directory, object_type):
 
 
 def load_and_label_data(dataset, resize_dim=0):
-    train_paths, test_paths, ground_truth_paths = dataset
+    train_paths, test_paths = dataset
 
     # Load all training images
+    train_images = []
+    train_labels = []
+    for path in train_paths['good']:
+        image = cv2.imread(path)
+        if resize_dim != 0:
+            image = cv2.resize(image, (resize_dim, resize_dim))
+        train_images.append(image)
+        train_labels.append(1)
     
+    for path in train_paths['bad']:
+        image = cv2.imread(path)
+        if resize_dim != 0:
+            image = cv2.resize(image, (resize_dim, resize_dim))
+        train_images.append(image)
+        train_labels.append(0)
+    
+    # Load all testing images
+    test_images = []
+    test_labels = []
+    for path in test_paths['good']:
+        image = cv2.imread(path)
+        if resize_dim != 0:
+            image = cv2.resize(image, (resize_dim, resize_dim))
+        test_images.append(image)
+        test_labels.append(1)
+    
+    for path in test_paths['bad']:
+        image = cv2.imread(path)
+        if resize_dim != 0:
+            image = cv2.resize(image, (resize_dim, resize_dim))
+        test_images.append(image)
+        test_labels.append(0)
+    
+    return np.array(train_images), np.array(train_labels), np.array(test_images), np.array(test_labels)
